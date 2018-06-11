e7c57895aa5142a60d2c0fc4e75f7b3ea2e43659
Digna Salcedo <didisalcedo1010@outlook.com>
2018-06-10 22:48:50 -0400

    pagina lista con formulario funcional

--- sendMail.php
+++ sendMail.php
@@ -1,32 +1,69 @@
 <?php
-if (isset($_POST["name"]) && isset($_POST["email"]))
-{
-	$name=$_POST['name'];
-	$email=$_POST['email'];
-	$to= 'salcedodigna7@gmail.com';
-$subject="Get My Free Quote";
-$body= '<html>
-					<body>
-						<h2>Feddback - </h2>
-						<hr>
-						<p>Name<br>'.$name.'</p>
-						<p>Email<br>'.$email.'</p>
-					</body>
-				</html>';
 
 
-				$headers = "From: ".$name." <".$email.">\r\n";
-				$headers .= "Reply-To: ".$email."\r\n";
-				$headers .= "MIME-Version: 1.0\r\n";
-				$headers .= "Content-type: text/html; charset=utf-8";
 
-				$send = mail($to,$subject,$body,$headers);
-				if ($send){
-					echo '<br>';
-					echo 'Gracias';
-				}
-				else{
-					echo 'Error';
-				}
-}
+
+//datos obtenidos
+$name=$_POST["name"];
+$phoneNumber=$_POST["phoneNumber"];
+$email=$_POST["email"];
+$service=$_POST["service"];
+$zipCode=$_POST["zipCode"];
+
+
+
+$to  = $email; // Email de la persona que envia el comentario
+
+
+// Asunto
+$subject = 'Get Your Free Quote';
+
+// message
+$message = '
+<html>
+<head>
+  <title>Get Your Free Quote</title>
+</head>
+<body>
+  <p><font color="blue">Contacto desde el portal web AM Project.</font></p>
+  <table>
+    <tr>
+      <td width="30%"><strong>Name:</strong></td>
+	  <td>'. $name .'</td>
+	</tr> 
+    <tr>
+      <td><strong>Tel&eacute;fono:</strong></td>
+	  <td>'. $phoneNumber .'</td>
+	</tr> 	
+	<tr>
+      <td><strong>Email:</strong></td>
+	  <td>'. $email .'</td>
+	</tr> 	
+	<tr>
+      <td><strong>Servicio:</strong></td>
+	  <td>'. $service .'</td>
+	</tr> 	
+	<tr>
+      <td><strong>Zip Code:</strong></td>
+	  <td>'. $zipCode .'</td>
+	</tr> 		
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To: AM Project <amprojectmiami@gmail.com>' . "\r\n";
+$headers .= 'From: Contacto desde el portal web AM Project <amprojectmiami@gmail.com>' . "\r\n";
+
+
+//Envio del email
+mail($to, $subject, $message, $headers);
+//Redireccion al home
+header("Location: http://www.amprojectflorida.com"); 
+
 ?>
